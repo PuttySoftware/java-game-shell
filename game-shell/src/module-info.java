d0a7d2db40c3f7dea94cd064f03bbe0ac183dc5b
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-10-20 14:05:31 -0400

    Remove unused dependency.
    The com.puttysoftware.integration module is not used by this module. Don't depend on it.

--- game-shell/src/module-info.java
+++ game-shell/src/module-info.java
@@ -8,7 +8,6 @@
 module com.puttysoftware.gameshell {
     requires transitive java.desktop;
     requires transitive com.puttysoftware.desktop;
-    requires com.puttysoftware.integration;
     exports com.puttysoftware.gameshell;
     exports com.puttysoftware.gameshell.dialogs;
     exports com.puttysoftware.gameshell.screens;
