fa8321a9af34532da5edd005d4a9c6dc2e5c8b16
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-06-07 13:54:22 -0400

    The Game Shell now builds atop Diane. Upgrade to Java SE 14.

--- game-shell/src/com/puttysoftware/gameshell/loaders/MusicCache.java
+++ game-shell/src/com/puttysoftware/gameshell/loaders/MusicCache.java
@@ -5,35 +5,35 @@ import java.util.ArrayList;
 import com.puttysoftware.audio.ogg.OggFactory;
 
 final class MusicCache {
-    // Fields
-    private ArrayList<MusicCacheEntry> cache;
-    private static final int INITIAL_SIZE = 10;
+	// Fields
+	private ArrayList<MusicCacheEntry> cache;
+	private static final int INITIAL_SIZE = 10;
 
-    // Constructor
-    MusicCache() {
-	this.cache = new ArrayList<>(MusicCache.INITIAL_SIZE);
-    }
-
-    // Methods
-    OggFactory getCachedMusic(final String name) {
-	for (MusicCacheEntry mce : this.cache) {
-	    if (mce.getName().equals(name)) {
-		return mce.getMusic();
-	    }
+	// Constructor
+	MusicCache() {
+		this.cache = new ArrayList<>(MusicCache.INITIAL_SIZE);
 	}
-	return null;
-    }
 
-    void addToCache(final String name, final OggFactory music) {
-	this.cache.add(new MusicCacheEntry(music, name));
-    }
-
-    boolean isInCache(final String name) {
-	for (MusicCacheEntry mce : this.cache) {
-	    if (mce.getName().equals(name)) {
-		return true;
-	    }
+	// Methods
+	OggFactory getCachedMusic(final String name) {
+		for (MusicCacheEntry mce : this.cache) {
+			if (mce.getName().equals(name)) {
+				return mce.getMusic();
+			}
+		}
+		return null;
 	}
-	return false;
-    }
+
+	void addToCache(final String name, final OggFactory music) {
+		this.cache.add(new MusicCacheEntry(music, name));
+	}
+
+	boolean isInCache(final String name) {
+		for (MusicCacheEntry mce : this.cache) {
+			if (mce.getName().equals(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- game-shell/src/module-info.java
+++ game-shell/src/module-info.java
@@ -7,9 +7,14 @@
  */
 module com.puttysoftware.gameshell {
     requires transitive java.desktop;
-    requires transitive com.puttysoftware.desktop;
     requires transitive com.puttysoftware.audio.ogg;
     requires transitive com.puttysoftware.audio.wav;
+    requires transitive com.puttysoftware.diane;
+    requires transitive com.puttysoftware.images;
+    requires transitive com.puttysoftware.polytable;
+    requires transitive com.puttysoftware.storage;
+    requires transitive com.puttysoftware.xio;
+    
     exports com.puttysoftware.gameshell;
     exports com.puttysoftware.gameshell.dialogs;
     exports com.puttysoftware.gameshell.loaders;
